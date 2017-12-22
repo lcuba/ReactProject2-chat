130ff365f91b4c67edd3a1add2994465818c065b
Lucas Cuba <lucas.cuba4@gmail.com>
2017-12-22 13:45:18 -0500

    checkpoint 3 work: added create room form

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,32 +6,59 @@ class RoomList extends Component {
     super(props)
 
     this.state = {
-      rooms: []
+      rooms: [],
+      handleForm: false,
+      newRoomName: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
-
+    this.createRoom = this.createRoom.bind(this);
   }
 
     componentDidMount() {
       this.roomsRef.on('child_added', snapshot => {
         const room = snapshot.val();
         room.key = snapshot.key;
-        console.log(snapshot.val());
         this.setState({rooms: this.state.rooms.concat(room)});
       });
+    }
+
+    formToggle() {
+      if (this.state.handleForm === true)
+        this.setState({handleForm: false});
+      else
+        this.setState({handleForm: true});
+    }
+
+    createRoom(e) {
+      e.preventDefault();
+      const newRoom = e.target.elements.newRoomName.value;
+      this.roomsRef.push({name: newRoom});
+      this.setState({handleForm: false});
     }
 
   render() {
     return (
       <section className='roomlist'>
         <h1 className='title'>Bloc Chat</h1>
+        <button className='new-room' onClick={this.formToggle.bind(this)}>New room</button>
         <ul className='sidebar-list'>
         {
           this.state.rooms.map((room, index) =>
             <li className='rooms' key={index}>{room.name}</li>
         )}
         </ul>
+        <form className={this.state.handleForm ? 'displayed' : 'hidden'} onSubmit={this.createRoom}>
+          <h2 className='form-title'>Create new room</h2>
+          <h3 className='text-field-description'>Enter a room name</h3>
+          <input
+            type='text'
+            id='new-room-name'
+            name='newRoomName'
+          />
+          <button className='cancel' onClick={this.formtoggle}>Cancel</button>
+          <button className='create-room' type='submit'>Create room</button>
+        </form>
       </section>
     );
   }
